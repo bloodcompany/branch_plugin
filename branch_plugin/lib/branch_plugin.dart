a03069aa3c8386182ca40b5c1320d2790f4a0377
aseem wangoo <aseemwangoo@gmail.com>
2020-03-10 18:42:13 +0800

    [C/Br] Include new method in plugin

--- branch_plugin/lib/branch_plugin.dart
+++ branch_plugin/lib/branch_plugin.dart
@@ -3,11 +3,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class BranchPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('branch_plugin');
+  static const MethodChannel _channel = const MethodChannel('branch_plugin');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static void reinitSession() {}
 }
