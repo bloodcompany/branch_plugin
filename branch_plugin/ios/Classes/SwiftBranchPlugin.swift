fc20535a74b1db124216297121d665b8a8d1ccb3
aseem wangoo <aseemwangoo@gmail.com>
2020-03-12 11:09:18 +0800

    [C/Br] Event handler ioS

--- branch_plugin/ios/Classes/SwiftBranchPlugin.swift
+++ branch_plugin/ios/Classes/SwiftBranchPlugin.swift
@@ -2,10 +2,16 @@ import Flutter
 import UIKit
 
 public class SwiftBranchPlugin: NSObject, FlutterPlugin {
+  static var eventHandler: EventStreamHandler?
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "branch_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftBranchPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+
+    let eventChannel = FlutterEventChannel(name: "branch_plugin/event", binaryMessenger: registrar.messenger())
+    eventHandler = EventStreamHandler()
+    eventChannel.setStreamHandler(eventHandler)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
