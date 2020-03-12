e1fbbd0a02c33cb3caed0ce4af0b7878e0682283
aseem wangoo <aseemwangoo@gmail.com>
2020-03-12 11:28:03 +0800

    [C/Br] Try another approach

--- branch_plugin/ios/Classes/SwiftBranchPlugin.swift
+++ branch_plugin/ios/Classes/SwiftBranchPlugin.swift
@@ -1,17 +1,17 @@
 import Flutter
 import UIKit
 
-public class SwiftBranchPlugin: NSObject, FlutterPlugin {
-  static var eventHandler: EventStreamHandler?
+var eventChannel: FlutterEventChannel?
+let EVENT_CHANNEL = "branch_plugin/event";
 
+public class SwiftBranchPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "branch_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftBranchPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
 
-    let eventChannel = FlutterEventChannel(name: "branch_plugin/event", binaryMessenger: registrar.messenger())
-    eventHandler = EventStreamHandler()
-    eventChannel.setStreamHandler(eventHandler)
+    eventChannel = FlutterEventChannel(name: EVENT_CHANNEL, binaryMessenger: registrar.messenger())
+    eventChannel!.setStreamHandler(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
