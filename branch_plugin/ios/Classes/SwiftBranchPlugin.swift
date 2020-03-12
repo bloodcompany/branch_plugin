5cfdd2ead429a799b569f3c2f73c718b790933b8
aseem wangoo <aseemwangoo@gmail.com>
2020-03-12 11:41:38 +0800

    [C/Br] Modify swift code

--- branch_plugin/ios/Classes/SwiftBranchPlugin.swift
+++ branch_plugin/ios/Classes/SwiftBranchPlugin.swift
@@ -4,14 +4,15 @@ import UIKit
 var eventChannel: FlutterEventChannel?
 let EVENT_CHANNEL = "branch_plugin/event";
 
-public class SwiftBranchPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+public class SwiftBranchPlugin: NSObject, FlutterPlugin, FlutterStreamHandler  {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "branch_plugin", binaryMessenger: registrar.messenger())
+    
     let instance = SwiftBranchPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-
     eventChannel = FlutterEventChannel(name: EVENT_CHANNEL, binaryMessenger: registrar.messenger())
     eventChannel!.setStreamHandler(instance)
+
+    registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
